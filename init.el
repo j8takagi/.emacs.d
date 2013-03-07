4acaf61b1dabc7d507f6197f5ea2bd221b3442d6
j8takagi <j8takagi@nifty.com>
2013-03-07 14:50:34 +0900

    Ruby-modeの記述を追加。常時run-rubyでirbを呼び出せるように

--- init.el
+++ init.el
@@ -549,3 +549,21 @@
 
 ;; svg-clock
 (autoload 'svg-clock "svg-clock" "Start/stop svg-clock" t)
+
+;; ruby-mode
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(setq auto-mode-alist
+      (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
+(setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
+                                     interpreter-mode-alist))
+(load "inf-ruby")
+
+;; set to load inf-ruby and set inf-ruby key definition in ruby-mode.
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (inf-ruby-keys)))
