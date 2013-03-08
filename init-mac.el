2d2e5e27815fd318fd0b79686e30b4edc2d06d8a
j8takagi <j8takagi@nifty.com>
2013-03-08 12:53:28 +0900

    mac-init.elを更新

--- init-mac.el
+++ init-mac.el
@@ -57,48 +57,9 @@
         "/usr/local/share/info/ja"
         "/usr/share/info/"))
 
-;; auto-installのフォルダを、site-lispフォルダに設定
-(setq auto-install-directory "/usr/local/share/emacs/site-lisp/auto-install/")
-
 ;; commandキーをEmacsのMetaキーとして設定
 (setq mac-command-modifier 'meta)
-;; ;; optionキーはEmacsでは使わない
+;; optionキーはEmacsでは使わない
 ;; (setq mac-option-modifier nil)
 ;; ほかのアプリと同様に、command + F1でアプリケーションの次のウィンドウを操作対象にする
 (global-set-key '[s-f1] 'other-frame)
-
-(defvar ex-open-cmd "open")
-
-;;; 指定したファイルをオープン
-(defun ex-open-file (filename)
-  "Open file with external program"
-  (let ((process-connection-type nil))
-    (start-process "external open" nil ex-open-cmd filename)))
-
-;;; バッファからファイルをオープン
-(defun ex-open-file-from-buffer ()
-  "Open the buffer file with external program"
-  (interactive)
-  (if buffer-file-name
-      (ex-open-file (buffer-file-name))
-    (error "This buffer is not visiting a file")))
-
-(global-set-key "\C-x\M-\C-f" 'ex-open-file-from-buffer)
-
-;; カーソル下のファイルやディレクトリを関連付けられたプログラムで開く
-(defun dired-ex-open-file ()
-  "Open the current file with external program"
-  (interactive)
-  (ex-open-file (dired-get-filename)))
-
-;; 現在のディレクトリを関連付けられたプログラムで開く
-(defun dired-cd-ex-open ()
-  "Open current directory with external program"
-  (interactive)
-  (ex-open-file (expand-file-name dired-directory)))
-
-;; キーバインド
-(add-hook 'dired-mode-hook
-          (lambda ()
-            (define-key dired-mode-map (kbd "r") 'dired-ex-open-file)
-            (define-key dired-mode-map (kbd "C-c .") 'dired-cd-ex-open)))
