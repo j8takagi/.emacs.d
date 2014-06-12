be8854866852749dbaaba7d5675848f4a123d177
j8takagi <j8takagi@nifty.com>
2014-06-12 12:47:30 +0900

    mmm-modeを設定

--- init.el
+++ init.el
@@ -482,6 +482,30 @@
 ;; js-mode
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 
+;; mmm-mode
+(load-library "mmm-mode")
+
+(require 'mmm-auto)
+
+(setq mmm-global-mode 'maybe)
+(setq mmm-submode-decoration-level 3)
+;(setq mmm-font-lock-available-p t)
+
+(set-face-background 'mmm-default-submode-face "#f0f0ff")
+
+(mmm-add-classes
+ '((embedded-css
+    :submode css-mode
+    :front "<style[^>]*>\n"
+    :back "\n?[ \t]+</style>")))
+(mmm-add-mode-ext-class nil "\\.html?\\'" 'embedded-css)
+
+(mmm-add-classes
+ '((html-javascript
+    :submode javascript-mode
+    :front "<script[^>]*>\n"
+    :back "[ \t]+</script>")))
+(mmm-add-mode-ext-class nil "\\.html?\\'" 'html-javascript)
 
 ;; image-mode
 (setq image-file-name-extensions '("png" "jpeg" "jpg" "gif" "tiff" "tif"))
