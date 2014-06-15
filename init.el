59a701226cddd1f52630592e065c59df93b829b9
j8takagi <j8takagi@nifty.com>
2014-06-15 14:26:33 +0900

    フレームとバッファーのサイズを調整する機能を追加

--- init.el
+++ init.el
@@ -685,3 +685,119 @@
           (lambda ()
             (setq Info-additional-directory-list Info-default-directory-list)
             ))
+
+;; window-resize
+;;
+;; from: window-resizer - http://d.hatena.ne.jp/khiker/20100119/window_resize
+(defun wctl-window-resize ()
+  "Control window size and position."
+  (interactive)
+    (let
+        ((thiswindow (selected-window))
+        (start-width (window-width))
+        (start-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1 -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1 -1))
+        action
+        c)
+      (catch 'end-flag
+      (while t
+        (setq action
+           (read-key-sequence-vector
+              (format
+                "Window Size current:[%dx%d]; start:[%dx%d]; type q to quit."
+                (frame-width) (frame-height) start-width start-height)))
+        (setq c (aref action 0))
+        (cond
+          ((= c ?l)
+            (enlarge-window-horizontally dx))
+          ((= c ?h)
+            (shrink-window-horizontally dx))
+          ((= c ?j)
+            (enlarge-window dy))
+          ((= c ?k)
+            (shrink-window dy))
+          ((= c ?s)
+            (progn
+              (set-frame-width thiswindow start-width)
+              (set-frame-height thiswindow start-height)))
+          ((= c ?q)
+            (progn
+              (message "Window Resize: quit")
+              (throw 'end-flag t))))))))
+
+(defun wctl-frame-resize ()
+   "frame size and position control."
+   (interactive)
+     (let
+         ((thisframe (selected-frame))
+         (start-width (frame-width))
+         (start-height (frame-height))
+         (default-width (cdr (assoc 'width default-frame-alist)))
+         (default-height (cdr (assoc 'height default-frame-alist)))
+         action
+         c)
+       (catch 'end-flag
+       (while t
+        (setq action
+           (read-key-sequence-vector
+              (format
+                "Frame Size current:[%dx%d]; start:[%dx%d]; default:[%dx%d]; type q to quit."
+                (frame-width) (frame-height)
+                start-width start-height default-width default-height)))
+        (setq c (aref action 0))
+        (cond
+          ((= c ?l)
+            (set-frame-width thisframe (+ (frame-width) 1)))
+          ((= c ?h)
+            (set-frame-width thisframe (- (frame-width) 1)))
+          ((= c ?j)
+            (set-frame-height thisframe (+ (frame-height) 1)))
+          ((= c ?k)
+            (set-frame-height thisframe (- (frame-height) 1)))
+          ((= c ?d)
+            (progn
+              (set-frame-width thisframe default-width))
+              (set-frame-height thisframe default-height))
+          ((= c ?s)
+            (progn
+              (set-frame-width thisframe start-width)
+              (set-frame-height thisframe start-height)))
+          ((= c ?q)
+            (progn
+              (message "Frame Resize: quit")
+              (throw 'end-flag t))))))))
+
+;; C-\ をプリフィックスキー化
+(define-key global-map "\C-\\" (make-sparse-keymap))
+
+;; C-\ wで、window-control
+(global-set-key "\C-\\w" 'wctl-window-resize)
+
+;; C-\ fで、frame-control
+(global-set-key "\C-\\f" 'wctl-frame-resize)
+
+
+(add-hook 'before-make-frame-hook 'frame-shift-right)
+
+(add-hook 'delete-frame-functions
+          (lambda (frame)
+            (frame-shift-left)))
+
+(defvar frame-shift-size 20)
+
+(defun frame-shift-right ()
+  (let* ((leftcell (assoc 'left default-frame-alist))
+        (topcell (assoc 'top default-frame-alist)))
+    (setcdr leftcell (+ (cdr leftcell) frame-shift-size))
+    (setcdr topcell (+ (cdr topcell) frame-shift-size))))
+
+(defun frame-shift-left ()
+  (let* ((leftcell (assoc 'left default-frame-alist))
+        (left (cdr leftcell))
+        (topcell (assoc 'top default-frame-alist))
+        (top (cdr topcell)))
+    (if (>= left frame-shift-size)
+        (setcdr leftcell (- left frame-shift-size)))
+    (if (>= top frame-shift-size)
+        (setcdr topcell (- top frame-shift-size)))))
