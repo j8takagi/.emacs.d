40e4767c3584be1f6e8f12ceeb5e3dfe62b48cca
j8takagi <j8takagi@nifty.com>
2014-06-24 16:16:29 +0900

    elpa/*/*.elを管理対象ファイルから除外

--- elpa/japanese-holidays-20140620.559/japanese-holidays-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; japanese-holidays-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("japanese-holidays-pkg.el" "japanese-holidays.el")
-;;;;;;  (21415 40596 307265 0))
-
-;;;***
-
-(provide 'japanese-holidays-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; japanese-holidays-autoloads.el ends here
