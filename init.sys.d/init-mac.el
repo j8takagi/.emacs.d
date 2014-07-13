6d136586249665240e5ce573c16ea0dfe5dec3fa
j8takagi <j8takagi@nifty.com>
2014-07-14 00:49:40 +0900

    フォントの設定方法を修正

--- init.sys.d/init-mac.el
+++ init.sys.d/init-mac.el
@@ -42,8 +42,20 @@
        ))
   (add-to-list 'default-frame-alist val))
 
-(set-fontset-font t 'japanese-jisx0208
-                  (font-spec :family "Hiragino Kaku Gothic ProN"))
+(dolist
+    (list
+     '(
+       (jisx0201 "Osaka")
+       (japanese-jisx0213.2004-1 "Hiragino Kaku Gothic ProN")
+       ))
+  (let ((charset (car list))
+        (fontfamily (nth 1 list)))
+    (cond
+     ((not (member charset charset-list))
+        (message "Character set %s is not found." charset))
+     ((not (member fontfamily (font-family-list)))
+        (message "Font family %s is not found." fontfamily))
+     ((set-fontset-font t charset (font-spec :family fontfamily))))))
 
 (setq default-input-method "MacOSX")
 
