cdf2e7a5dff6a335b3e0a1369a1eb887748cec3c
j8takagi <j8takagi@nifty.com>
2014-07-14 19:01:52 +0900

    Windows用の設定ファイルを修正

--- init.sys.d/init-w32.el
+++ init.sys.d/init-w32.el
@@ -1,5 +1,5 @@
 ;; -*- mode: Emacs-Lisp; -*-
-;;; MS-Windowsの設定
+;; MS-Windowsの設定
 
 ;; フレームの設定
 (dolist
@@ -7,18 +7,31 @@
      '(
        (width . 120)
        (height . 34)
-       (top . 22)
+       (top . 0)
        (left . 0)
-       )))
+       ))
+  (add-to-list 'default-frame-alist val))
 
-;; 英語フォントの設定
+;; フォントサイズと英語フォントの設定
 (set-face-attribute 'default nil
-                     :family "Consolas"
-                     :height 120)
+                    :height 120
+                    :family "Consolas")
 
 ;; 日本語フォントの設定
-(set-fontset-font t 'japanese-jisx0208
-                  (font-spec :family "Meiryo"))
+(dolist
+    (list
+     '(
+       (jisx0201 "ＭＳ ゴシック")
+       (japanese-jisx0213.2004-1 "メイリオ")
+       ))
+  (let ((charset (car list))
+        (fontfamily (nth 1 list)))
+    (cond
+     ((not (member charset charset-list))
+        (message "Character set '%s' is not found." charset))
+     ((not (member fontfamily (font-family-list)))
+        (message "Font family '%s' is not found." fontfamily))
+     ((set-fontset-font t charset (font-spec :family fontfamily))))))
 
 ;; 文字コードのデフォルトはUTF-8
 (prefer-coding-system 'utf-8-dos)
@@ -27,7 +40,8 @@
 (setq default-file-name-coding-system 'shift_jis)
 
 ;; emacsclientを使えるように
-(eval-after-load "server" '(server-start))
+(eval-after-load "server"
+  '(unless (server-running-p) (server-start)))
 
 ;; 環境変数EDITORの設定
 (setenv "EDITOR" "emacsclient")
--- setup.bat
+++ setup.bat
@@ -1,9 +1,13 @@
+emacs -batch -l recompile-elpa.el
 set EMACSDIR=%HOME%\.emacs.d
-rmdir /S /Q %EMACSDIR%
-mkdir %EMACSDIR%
+copy %EMACSDIR%\init.el %EMACSDIR%\init.el~
 copy init.el %EMACSDIR%\init.el
-copy init-w32.el %EMACSDIR%\init-w32.el
-mkdir %EMACSDIR%\site-lisp
-xcopy /s /EXCLUDE:setup-exclude.txt "site-lisp" "%EMACSDIR%\site-lisp\"
+rmdir /S /Q %EMACSDIR%\init.d
+xcopy /s /EXCLUDE:setup-exclude.txt "init.d" "%EMACSDIR%\init.d"
+rmdir /S /Q %EMACSDIR%\init.sys.d
+mkdir %EMACSDIR%\init.sys.d
+copy init-w32.el %EMACSDIR%\init.sys.d\init-w32.el
+rmdir /S /Q %EMACSDIR%\site-lisp
+xcopy /s /EXCLUDE:setup-exclude.txt "site-lisp" "%EMACSDIR%\site-lisp"
 mkdir %EMACSDIR%\elpa
-xcopy /s "elpa" "%EMACSDIR%\elps\"
+xcopy /s "elpa" "%EMACSDIR%\elpa"
