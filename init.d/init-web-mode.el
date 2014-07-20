6e75f500ef5bd2707b638e5f3473ba981c6ac386
j8takagi <j8takagi@nifty.com>
2014-07-20 14:31:41 +0900

    HTMLタグのインデントを0に

--- init.d/init-web-mode.el
+++ init.d/init-web-mode.el
@@ -1,6 +1,6 @@
 (require 'web-mode)
 
-;; web-modeの色設定
+;; web-modeのface設定
 (custom-set-faces
  '(web-mode-indent-style 1)
  '(web-mode-comment-face ((t (:foreground "#D9333F"))))
@@ -10,4 +10,10 @@
  '(web-mode-html-tag-face ((t (:foreground "#00c0e0" :weight bold))))
  '(web-mode-server-comment-face ((t (:foreground "#D9333F")))))
 
+;; HTMLタグのインデントを0に
+(defun web-mode-init ()
+  "Set configuration to Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 0))
+
+(add-hook 'web-mode-hook 'web-mode-init)
 (provide 'init-web-mode)
