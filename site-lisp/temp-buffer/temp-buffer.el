dff9d32e03b2d6cc069f61db2bc1ed01be1cad8d
j8takagi <j8takagi@nifty.com>
2014-08-28 18:34:03 +0900

    temp-bufferで、バッファをtext-modeになるよう仕様変更

--- site-lisp/temp-buffer/temp-buffer.el
+++ site-lisp/temp-buffer/temp-buffer.el
@@ -1,11 +1,12 @@
 ;; テンポラリバッファを開く
 (defun switch-to-temp-buffer ()
-  "Create temporary buffer."
+  "Create temporary text-mode buffer."
   (interactive)
   ;; バッファ名は現在の日時
   (switch-to-buffer
    (generate-new-buffer
-    (concat "*" (replace-regexp-in-string " +" "_" (current-time-string)) "*"))
-   (setq buffer-offer-save nil)))
+    (concat (replace-regexp-in-string " +" "_" (current-time-string)) ".txt")))
+  ;; text-modeを設定
+  (text-mode))
 
 (provide 'temp-buffer)
