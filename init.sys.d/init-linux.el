3359ca5ecda5536be5547e3bd8a249b32e3f6389
j8takagi <j8takagi@nifty.com>
2017-06-03 18:53:26 +0900

    Ubuntuのフォントなどの設定を反映

--- init.sys.d/init-linux.el
+++ init.sys.d/init-linux.el
@@ -1,8 +1,15 @@
 ;; -*- mode: Emacs-Lisp; -*-
 ;; Linuxの設定
+
 (message "Start of loading %s." load-file-name)
 
 ;; 環境変数LANGの設定
-(setenv "LANG" "en_US.UTF-8")
+(my-init-setenv
+ '("LANG" "en_US.UTF-8")
+ '("PAGER" "cat")
+ '("MANPAGER" "cat")
+ '("EDITOR" "emacsclient")
+ '("VISUAL" "emacsclient")
+ )
 
 (provide 'init-linux)
--- init.sys.d/init-x.el
+++ init.sys.d/init-x.el
@@ -3,28 +3,33 @@
 
 (message "Start of loading %s." load-file-name)
 
+(require 'my-init)
+
+(my-init-requires
+ 'fontset-set
+ )
+
 ;; 標準のフォントサイズとフォントファミリーの設定
-(set-face-attribute 'default nil
-                    :height 120)
+(my-init-custom-set-alist
+ `(default-frame-alist                  ; デフォルトフレーム
+    (font
+     ,(fontset-set
+       '(
+         (ascii . (font-spec :family "Inconsolata" :weight 'normal :slant 'normal :size 14))
+         (unicode . (font-spec :family "TakaoExゴシック"))
+         )
+       "mydefault_x"))
+    (width 180)
+    (height 50)
+    (top 0)
+    (left 0)
+    ))
 
-;; 日本語フォントにIPAゴシックフォントを設定
-(dolist
-    (charfont                           ; キャラクタセットごとのフォントファミリー
-     '(
-       (japanese-jisx0213.2004-1 "IPAexゴシック")
-       (japanese-jisx0213-2 "IPAexゴシック")
-       ))
-  (my-init-set-fontfamily (car charfont) (cadr charfont)))
-
-;; フレームの設定
-(dolist
-    (fparam                             ; フレームパラメーター
-     '(
-       (width 150)
-       (height 40)
-       (top 0)
-       (left 0)
-       ))
-  (add-to-list 'default-frame-alist (cons (car fparam) (cadr fparam))))
+;; view-modeの設定
+(with-eval-after-load 'view
+  (my-init-custom-set-list
+   '(set-view-mode-read-write-directory-patterns
+     "/media/.+/Documents/" "~/.emacs.d/elpa" "/tmp" "/var"
+     )))
 
 (provide 'init-x)
