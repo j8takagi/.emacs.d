fa42acc035ad07001e2e552c7b4715877b1fad48
j8takagi <j8takagi@nifty.com>
2017-06-07 18:16:31 +0900

    windowsで日本語ファイル名が文字化けする問題を修正
    default-process-coding-systemをprefer-coding-systemのあとで設定する

--- init.sys.d/init-w32.el
+++ init.sys.d/init-w32.el
@@ -7,6 +7,9 @@
 
 (listify-requires 'fontset-set)
 
+;; 文字コードのデフォルトはUTF-8
+(prefer-coding-system 'utf-8-dos)
+
 ;; 環境変数EDITORの設定
 (listify-setenv
  '("EDITOR" "emacsclient")
@@ -26,11 +29,17 @@
     (height 48)
     (top 0)
     (left 0)
-    ))
- '(default-file-name-coding-system cp932) ; 日本語ファイル名を正常に処理する
- '(default-input-method "W32-IME")                ; IMEの設定
- '(default-process-coding-system (utf-8 cp932)) ; 日本語ファイル名を正常に処理する
+    )))
+
+(listify-set
+ '(default-file-name-coding-system cp932) ; 日本語ファイル名を正常に処理
+ '(default-process-coding-system (utf-8 . cp932)) ; 日本語ファイル名を正常に処理
+ '(default-input-method "W32-IME")              ; IMEの設定
  )
+
+;; Shell-modeの文字コード設定
+(defun set-buffer-process-coding-system-cp932 ()
+  (set-buffer-process-coding-system 'cp932 'cp932))
 
 ;; view-modeの設定
 (with-eval-after-load 'view
@@ -43,13 +52,6 @@
  '("<M-kanji>" ignore)             ; IME切り替え時に undefined のエラーメッセージが表示されるのを抑制
  '("<kanji>" toggle-input-method)
  )
-
-;; Shell-modeの文字コード設定
-(defun set-buffer-process-coding-system-cp932 ()
-  (set-buffer-process-coding-system 'cp932 'cp932))
-
-;; 文字コードのデフォルトはUTF-8
-(prefer-coding-system 'utf-8-dos)
 
 ;; フックの設定
 (listify-set
