584d89694f15ebe0fc38097cbafe7120ae0a4a7a
j8takagi <j8takagi@nifty.com>
2017-08-24 09:13:30 +0900

    MacOSXのフォント設定を変更

--- init.sys.d/init-mac-gui.el
+++ init.sys.d/init-mac-gui.el
@@ -22,23 +22,35 @@
 (listify-set
  `(default-frame-alist                  ; デフォルトフレーム
     (
+     (width 180)
+     (height 56)
+     (top 23)
+     (left 0)
      (font
       ,(fontsets-set
         '((
           (ascii . (font-spec :family "Menlo" :weight 'normal :slant 'normal :size 12))
           (unicode . (font-spec :family "Hiragino Sans"))
           ) "my_default_mac")))
-     (width 180)
-     (height 56)
-     (top 23)
-     (left 0)
      ))
- '(face-font-rescale-alist (("Hiragino Sans" 1.167) ("YuGothic" 1.167)))
+; '(face-font-rescale-alist (("Hiragino Sans" 1.167) ("YuGothic" 1.167)))
  '(mac-command-modifier meta)        ; commandキーをEmacsのMetaキーに
  '(mac-auto-ascii-mode t) ; ミニバッファへのカーソル移動時、日本語IMEを自動オフ
  )
 
 (fontsets-set
+ '((
+    (ascii . (font-spec :family "Menlo" :weight 'bold :slant 'normal :size 12))
+    (unicode . (font-spec :family "Hiragino Sans" :weight 'bold :slant 'normal))
+    ) "my_bold")
+ '((
+    (ascii . (font-spec :weight 'normal :slant 'italic))
+    (unicode . (font-spec :family "Hiragino Sans" :weight 'semibold :slant 'normal))
+    ) "my_italic")
+ '((
+    (ascii . (font-spec :family "Menlo" :weight 'bold :slant 'italic :size 12))
+    (unicode . (font-spec :family "Hiragino Sans" :weight 'bold :slant 'normal))
+    ) "my_bold_italic")
  '((
     (unicode . (font-spec :family "Hiragino Sans" :weight 'normal :slant 'normal :size 12))
     ) "my_gothic")
@@ -52,22 +64,28 @@
     (unicode . (font-spec :family "YuMincho" :weight 'normal :slant 'normal :size 12))
     ) "my_yu_mincho")
  '((
-    (ascii . (font-spec :family "Menlo" :weight 'normal :slant 'normal :size 12))
-    (unicode . (font-spec :family "YuGothic" :weight 'normal :slant 'normal))
+    (ascii . (font-spec :family "Menlo" :weight 'normal))
+    (unicode . (font-spec :family "YuGothic"))
     ) "my_yu_gothic")
  '((
-    (unicode . (font-spec :family "Toppan Bunkyu Gothic" :weight 'normal :slant 'normal :size 12))
+    (unicode . (font-spec :family "Toppan Bunkyu Gothic"))
     ) "my_bunkyu_gothic")
  '((
-    (unicode . (font-spec :family "Hiragino Maru Gothic ProN" :weight 'normal :slant 'normal :size 12))
+    (unicode . (font-spec :family "Hiragino Maru Gothic ProN"))
     ) "my_maru_gothic")
  '((
-    (unicode . (font-spec :family "Klee" :weight 'normal :slant 'normal :size 12))
+    (unicode . (font-spec :family "Klee"))
     ) "my_klee")
  '((
-    (unicode . (font-spec :family "Source Han Code JP" :weight 'normal :slant 'normal :size 12))
+    (unicode . (font-spec :family "Source Han Code JP"))
     ) "my_sourcehancode")
+ '((
+   (ascii . (font-spec :family "Source Han Code JP" :weight 'normal :slant 'normal :size 12))
+   (unicode . (font-spec :family "Toppan Bunkyu Gothic" :weight 'normal :slant 'normal))
+    ) "my_alternative")
  )
+
+(set-face-fontset 'italic "-*-*-*-*-*-*-*-*-*-*-*-*-fontset-my_italic")
 
 ;; view-modeの設定
 (with-eval-after-load 'set-view-mode
