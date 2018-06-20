74de884f2390f03a5528a1c873a41fd965de97c6
j8takagi <j8takagi@nifty.com>
2018-06-20 22:24:39 +0900

    init.sys.dディレクトリのファイルを正常コンパイルできるよう修正

--- init.sys.d/set-compile.el
+++ init.sys.d/set-compile.el
@@ -1,5 +1,18 @@
-(let ((default-directory (expand-file-name "..")))
-  (normal-top-level-add-subdirs-to-load-path))
+(normal-top-level-add-subdirs-to-load-path)
 
 (require 'package)
 (package-initialize)
+
+(require 'server)
+
+(add-to-list 'load-path default-directory)
+(add-to-list 'load-path "../site-lisp/listify")
+(add-to-list 'load-path "../site-lisp/font-utilities")
+
+(require 'listify)
+(require 'fontset-set)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
