2a460e7d3316e21bc5c6eda192257a5b819953da
j8takagi <j8takagi@nifty.com>
2018-08-02 16:19:56 +0900

    ~/.emacs.d/abbrev_defsがない場合の異常終了を回避

--- insert/load-abbrev.el
+++ insert/load-abbrev.el
@@ -1,1 +1,8 @@
-(load-file "~/.emacs.d/abbrev_defs")
+(let ((abbrevfile "~/.emacs.d/abbrev_defs"))
+  (when (file-exists-p abbrevfile)
+    (load-file "~/.emacs.d/abbrev_defs")))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
