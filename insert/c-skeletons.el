0eb7f4f78c1966d81060a2fdab2150761ca1a10f
j8takagi <j8takagi@nifty.com>
2019-02-12 12:08:03 +0900

    skeltonに、C言語のwhile、switchを追加

--- insert/c-skeletons.el
+++ insert/c-skeletons.el
@@ -54,5 +54,31 @@
   > "}" ?\n
   )
 
+(define-skeleton c-dowhile
+  "do while statement in c-mode."
+  > "do {" ?\n
+  >  _ ?\n
+  > "} while( );" ?\n
+  )
+
+(define-skeleton c-switch
+  "switch statement in c-mode."
+  > "switch(" _ ") {" ?\n
+  > "case :" ?\n
+  >  ?\n
+  > "break;" ?\n
+  > "default:" ?\n
+  >  ?\n
+  > "break;" ?\n
+  > "}" ?\n
+  )
+
+(define-skeleton c-case
+  "case statement of switch in c-mode."
+  > "case :" ?\n
+  >  ?\n
+  > "break;" ?\n
+  )
+
 (provide 'c-skeletons)
 ;;; c-skeletons.el ends here
