2ff32c8bcd17770386fc9b14f07d313755e9a964
j8takagi <j8takagi@nifty.com>
2022-03-28 17:13:11 +0900

    EmacsからQuick Lookupを使う設定

--- init.sys.d/init-ns-gui.el
+++ init.sys.d/init-ns-gui.el
@@ -112,6 +112,32 @@
 
 (listify-requires 'dictionary_app)
 
+;; (listify-set
+;;  '(mac-selected-keyboard-input-source-change-hook mac-ime-cursor-change-color)
+;;  '(focus-in-hook mac-ime-cursor-change-color)
+;;  )
+
+;;; ファイルをQuick Lookupで開く
+(defun quicklookup (file)
+  "Quick look a file in Mac OS."
+  ;;; 指定したファイルが実在しない場合はエラー終了
+  (unless (file-exists-p file)
+    (error "%s: File does not exist." file))
+  (let (cmdstr (procname "quicklookup"))
+    (setq cmdstr
+          (mapconcat 'identity
+                     (list "qlmanage" "-p" (shell-quote-argument file))
+                     " "))
+    (message "%s at %s: %s" procname (format-time-string "%Y/%m/%d %H:%M:%S") cmdstr)
+    (start-process-shell-command procname (messages-buffer) cmdstr)))
+
+;; dired-modeでカーソル下のファイルやディレクトリーをQuick Lookupで開く
+(defun dired-quicklookup ()
+  "Open file mentioned on this line in Quick Lookup."
+  (interactive)
+  (quicklookup (dired-get-filename)))
+
+
 ;; Mac OS Xのキー設定
 (listify-global-set-keys
  '("<M-f1>" other-frame)    ; Mac OS Xの他アプリと同様に、command + F1でアプリケーションの次のウィンドウを操作対象にする
