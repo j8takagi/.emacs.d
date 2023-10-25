e7f078511bc26949dae64ac67b535888bbb79cf9
j8takagi <j8takagi@nifty.com>
2023-10-26 00:30:45 +0900

    count-japaneseで、指定した範囲の文字数を数えるよう仕様変更

--- site-lisp/count-japanese/count-japanese.el
+++ site-lisp/count-japanese/count-japanese.el
@@ -1,6 +1,22 @@
 ;; 日本語を数える
-(defun count-japanese ()
-  (interactive)
-  (message "日本語の文字数: %d字" (how-many "\\cj" (point-min) (point-max))))
+(defun count-japanese-do (start end)
+  (message "日本語の文字数: %d字" (how-many "\\cj" start end)))
+
+(defun count-japanese (start end &optional arg)
+  "Count the number of Japanese characters in the region.
+If called interactively, print a message reporting the number of
+Japanese characters in the region (whether or not the
+region is active); with prefix ARG, report for the entire buffer
+rather than the region."
+  (interactive (if current-prefix-arg
+           (list nil nil current-prefix-arg)
+         (list (region-beginning) (region-end) nil)))
+  (cond
+   ((not (called-interactively-p 'any))
+    (count-japanese-do start end))
+   (arg
+    (count-japanese-do (point-min) (point-max)))
+   (t
+    (count-japanese-do start end))))
 
 (provide 'count-japanese)
