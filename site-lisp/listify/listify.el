9cfefad68c00197ad087a755edad99dad1ce62f8
j8takagi <j8takagi@nifty.com>
2023-11-02 17:05:33 +0900

    set-minor-modesとset-hooksを追加

--- site-lisp/listify/listify.el
+++ site-lisp/listify/listify.el
@@ -30,9 +30,11 @@ Each VALUE-NEW-OLD-ALIST has the form (VALUE-NEW . VALUE-OLD)."
     alst))
 
 (defun listify-validate-custom-variable-type (custom-variable &optional value)
-  "Varidate VALUES is match CUSTOM-VARIABLE to custom-variable-type in symbol property.
+  "Varidate VALUES is match CUSTOM-VARIABLE to
+custom-variable-type in symbol property.
 If VALUE matches custom-variable-type in symbol properties list, t.
-When VALUE is ommited or nil, current value of CUSTOM-VARIABLE is validated."
+When VALUE is ommited or nil, current value of
+CUSTOM-VARIABLE is validated."
   (let (atype)
     (when (null value)
       (setq value (symbol-value custom-variable)))
@@ -46,10 +48,13 @@ When VALUE is ommited or nil, current value of CUSTOM-VARIABLE is validated."
 
 (defvar system-name-simple
   (replace-regexp-in-string "\\..*\\'" "" (system-name))
-  "The simple host name of the machine Emacs is running on, which is without domain information.")
+  "The simple host name of the machine Emacs is running on,
+which is without domain information.")
 
 (defun listify-requires (&rest feature)
-  "Require FEATURE, and the result is printed into the `*Messages' buffer, or  the standard error stream in batch mode."
+  "Require FEATURE, and the result is printed
+into the `*Messages' buffer,
+or the standard error stream in batch mode."
   (let (feats)
     (dolist (afeat feature)
       (if (featurep afeat)
@@ -62,7 +67,9 @@ When VALUE is ommited or nil, current value of CUSTOM-VARIABLE is validated."
     (message "Features are required - %s" feats)))
 
 (defun listify-requires-by-system (&rest sys-features)
-  "If current system type or window system got by VARIABLE is match to SYSTEM, Require FEATURE by `listify-requires' in SYS-FEATURES.
+  "If current system type or window system got by VARIABLE is match to SYSTEM,
+Require FEATURE by `listify-requires' in SYS-FEATURES.
+
 Each element of SYS-FEATURES has the form (VARIABLE SYSTEM FEATURE)."
   (dolist (asysfeat sys-features)
     (when (equal (eval (car asysfeat)) (cadr asysfeat))
@@ -72,7 +79,9 @@ Each element of SYS-FEATURES has the form (VARIABLE SYSTEM FEATURE)."
   "Define autoload functions from FUNC-FILE-DOC.
 Each FUNC-FILE-DOC has the form (FUNCTION FILE DOC).
 
-If FUNCTION is void or FILE is not found, warning message is printed into the `*Messages' buffer, or  the standard error stream in batch mode."
+If FUNCTION is void or FILE is not found,
+warning message is printed into the `*Messages' buffer,
+or the standard error stream in batch mode."
   (let (funcs ffd)
     (dolist (ffd func-file-doc)
       (let ((afunc (car ffd)) (afile (cadr ffd)) (adoc (nth 2 ffd)))
@@ -90,7 +99,9 @@ If FUNCTION is void or FILE is not found, warning message is printed into the `*
 
 (defun listify-set-alist (&rest alist-args)
   "Custom set ALIST-ARGS value to the alist.
-Each ALIST-ARGS has the form (ALIST-NAME ((KEY1 VALUE1) (KEY2 VALUE2) ...)[ NOW[ REQUEST[ COMMENT]]])."
+Each ALIST-ARGS has the form:
+
+(ALIST-NAME ((KEY1 VALUE1) (KEY2 VALUE2) ...)[ NOW[ REQUEST[ COMMENT]]])."
   (let (asym keyvals alsts)
     (dolist (alst alist-args)
       (setq asym (car alst) keyvals (symbol-value asym))
@@ -106,7 +117,9 @@ Each ALIST-ARGS has the form (ALIST-NAME ((KEY1 VALUE1) (KEY2 VALUE2) ...)[ NOW[
 
 (defun listify-set-list (&rest list-val)
   "Set LIST-VAL value to the list.
-Each LIST-VAL has the form (LIST-NAME (VALUE1 VALUE2 ...)[ NOW[ REQUEST[ COMMENT]]])."
+Each LIST-VAL has the form
+
+(LIST-NAME (VALUE1 VALUE2 ...)[ NOW[ REQUEST[ COMMENT]]])."
   (let (vals exps asym lsts)
     (dolist (lst list-val)
       (unless (listp (setq vals (cadr lst)))
@@ -130,35 +143,39 @@ The ARGS form is same to `custom-set-variables'.
 Except EXP need no quote when EXP is SYMBOL,
 and/or add each element when EXP is list,
 update or add each element when EXP is association list (alist)."
-  (let (asym aexp anow areq acomm vars)
+  (let (asym aexp anow areq acomm vars oldval)
     (dolist (arg args)
       (setq
        asym (nth 0 arg) aexp (nth 1 arg) anow (nth 2 arg) areq (nth 3 arg)
        acomm (listify-create-variable-comment asym (nth 4 arg)))
-      (if (and aexp (listp aexp) (listp (cdr aexp)))
-          (setq vars
-                (append
-                 vars
-                 (funcall
-                  (if (consp (car aexp))
-                      'listify-set-alist
-                    'listify-set-list)
-                  (list asym aexp anow areq acomm))))
-        (unless (listify-validate-custom-variable-type asym aexp)
-          (message
-           "%s: Variable type is mismatch.\nType: %s\nValue: %s"
-           asym (custom-variable-type asym) aexp))
-        (unless (equal (symbol-value asym) aexp)
-          (custom-set-variables
-           (list
-            asym
-            (if (and aexp (not (eq aexp t)) (or (symbolp aexp) (listp aexp)))
-                `(quote ,aexp)
-              aexp)
-            anow
-            areq
-            acomm)))
-        (setq vars (append vars (list asym)))))
+      (setq oldval (eval asym))
+      (unless (equal aexp oldval)
+        (if (and aexp (listp aexp) (listp (cdr aexp)))
+            (setq vars
+                  (append
+                   vars
+                   (funcall
+                    (if (consp (car aexp))
+                        'listify-set-alist
+                      'listify-set-list)
+                    (list asym aexp anow areq acomm))))
+          (unless (listify-validate-custom-variable-type asym aexp)
+            (message
+             "%s: Variable type is mismatch.\nType: %s\nValue: %s"
+             asym (custom-variable-type asym) aexp))
+          (if (equal (symbol-value asym) aexp)
+              (message "Variable `%s': value `%s' is not changed." asym oldval)
+            (custom-set-variables
+             (list
+              asym
+              (if (and aexp (not (eq aexp t)) (or (symbolp aexp) (listp aexp)))
+                  `(quote ,aexp)
+                aexp)
+              anow
+              areq
+              acomm)))
+          (message "Variable `%s': value `%s' is changed to `%s'." asym oldval (eval asym))
+          (setq vars (append vars (list asym))))))
     (listify-message-variables vars)
     vars))
 
@@ -218,10 +235,13 @@ or the standard error stream in batch mode."
 
 (defun listify-modemap-set-keys (&rest modemap)
   "Give KEY binding of MODEMAP as MAPKEYS after LIBRARY is loaded.
-MODEMAP form is (MODEMAP LIBRARY HOOK ((KEY1 FUNCTION1) (KEY2 FUNCTION2) ... )).
+Each MODEMAP has the form:
+
+(MODEMAP LIBRARY HOOK ((KEY1 FUNCTION1) (KEY2 FUNCTION2) ... )).
 
 If HOOK is not nil, the binding is via the HOOK.
-If function in MAPKEYS is void, warning message is printed into the `*Messages' buffer, or  the standard error stream in batch mode."
+If function in MAPKEYS is void, warning message is printed
+into the `*Messages' buffer, or  the standard error stream in batch mode."
   (let (amap alib ahook keyfuncs amapname funcdef)
     (dolist (amodemap modemap)
       (setq
@@ -242,15 +262,26 @@ If function in MAPKEYS is void, warning message is printed into the `*Messages' 
             (fset func-add-keybind funcdef)
             `(add-hook ',ahook ',func-add-keybind)))))))
 
-(defun listify-set-modes (&rest modeval)
-  "Set MODE. MODE format is assumed as `(FUNCTION 1)' to enable the mode, or `(FUNCTION 0)' to disable the mode. FUNCTION presents minor mode.
+(defun listify-set-minor-modes (&rest modeval)
+  "Set MODES. MODE format is assumed as `(MINOR-MODE-FUNCTION ARG)'.
+MINOR-MODE-FUNCTION presents minor mode.
+Usually, `(MINOR-MODE-FUNCTION 1)' to enable the mode,
+and `(MINOR-MODE-FUNCTION -1)' to disable the mode.
 
-If FUNCTION in MODE is void, warning message is printed into the `*Messages' buffer, or  the standard error stream in batch mode."
-  (let (amode)
+If FUNCTION in MODE is void, warning message is printed
+into the `*Messages' buffer,
+or the standard error stream in batch mode."
+  (let (amode oldval newval)
     (dolist (amodeval modeval)
-      (if (not (fboundp (setq amode (car amodeval))))
+      (setq amode (car amodeval))
+      (if (not (fboundp amode))
           (message "Warning: In setting minor mode, function `%s' is void." amode)
-        (eval amodeval)))))
+        (setq oldval (eval amode))
+        (eval amodeval)
+        (setq newval (eval amode))
+        (if (equal oldval newval)
+            (message "Info: minor mode - `%s': value `%s' is not changed." amode oldval)
+          (message "Minor mode - `%s': value `%s' is changed to `%s'." amode oldval newval))))))
 
 (defun listify-overwrite-auto-mode-alist (&rest mode-new-old)
   (let (anew aold alst)
@@ -263,20 +294,39 @@ If FUNCTION in MODE is void, warning message is printed into the `*Messages' buf
        (t
         (setq alst (push (cons anew aold) alst)))))
     (overwrite-values-alist 'auto-mode-alist alst)
-    (message "auto-mode-alist is overwritten.")))
+    (message "auto-mode-alist: value `%s' is changed to `%s'." aold anew)))
 
-(defun listify-set-hooks (&rest hook-func)
-  "Add function to hooks by list of (FUNCTION HOOK).
-If FUNCTION or HOOK is void, warning message is printed into the `*Messages' buffer, or  the standard error stream in batch mode."
-  (let (ahook afunc)
-    (dolist (ahookfunc hook-func)
-      (cond
-       ((not (boundp (setq ahook (car ahookfunc))))
-        (message "Warning: In setting hooks, hook `%s' is void." ahook))
-       ((not (fboundp (setq afunc (cadr ahookfunc))))
-        (message "Warning: In setting hooks, function `%s' is void." afunc))
-       (t
-          (add-hook ahook afunc))))))
+(defun listify-set-hook (hook func)
+  "Add a function to hook by list of (HOOK FUNCTION).
+If FUNCTION or HOOK is void,
+warning message is printed into the `*Messages' buffer,
+or the standard error stream in batch mode."
+  (cond
+   ((not (boundp hook))
+    (message "Warning: In setting hook, hook `%s' is void." hook))
+   ((not (fboundp func))
+    (message "Warning: In setting hook, function `%s' is void." func))
+   (t
+    (if (member func (eval hook))
+        (message "hook - `%s': function `%s' is already member." hook func)
+      (add-hook hook func)))))
+
+(defun listify-set-hooks (&rest hook-funcs)
+  "Add a function to hook by list:
+
+(HOOK1 (FUNC11 FUNC12 ...) (HOOK2 (FUNC21 FUNC22 ...) ...)
+
+If FUNCTION or HOOK is void,
+warning message is printed into the `*Messages' buffer,
+or the standard error stream in batch mode."
+  (dolist (ahookfuncs hook-funcs)
+    (let ((ahook (car ahookfuncs)) (funcs (cadr ahookfuncs)) oldval newval)
+      (dolist (afunc funcs)
+        (setq oldval (eval ahook))
+        (listify-set-hook ahook afunc))
+      (if (equal oldval (setq newval (eval ahook)))
+          (message "hook `%s' is not changed." ahook)
+        (message "hook `%s': value `%s' is changed to `%s'." ahook oldval newval)))))
 
 (defun listify-setenv (&rest env-val)
   "Add system environment variables to values by list of ENV-VAL.
