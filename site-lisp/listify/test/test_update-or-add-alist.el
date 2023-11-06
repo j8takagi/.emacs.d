8bf224cb5ddaea666cd8d51c2ce2c11e2cfbe91b
j8takagi <j8takagi@nifty.com>
2023-11-06 12:13:11 +0900

    update-or-add-alist関数単体テストの変更

--- site-lisp/listify/test/test_update-or-add-alist.el
+++ site-lisp/listify/test/test_update-or-add-alist.el
@@ -14,18 +14,32 @@
 (load-file "../listify.el")
 
 
-(ert-deftest listify-test-update-or-add-alist-1 ()
+(ert-deftest listify-test-update-or-add-alist_nil ()
   "Test `update-or-add-alist'.
-update a value of quoted symbol key. "
-  (let ((abc '((1 . "a") (b . "b") ("c" . "c"))))
-    (should (equal (update-or-add-alist 'abc 'b "B") '((1 . "a") (b . "B") ("c" . "c"))))
+Insert a value to null alist."
+  (let ((abc nil))
+    (should (equal (update-or-add-alist 'abc 1 "a") '((1 . "a"))))
     ))
 
-(ert-deftest listify-test-update-or-add-alist-2 ()
+(ert-deftest listify-test-update-or-add-alist_same_element ()
   "Test `update-or-add-alist'.
-update a value of integer key."
-  (let ((abc '((1 . "a") (b . "b") ("c" . "c"))))
-    (should (equal (update-or-add-alist 'abc 1 "A") '((1 . "A") (b . "b") ("c" . "c"))))
+Do not change when the element is a member of alist."
+  (let ((abc '((1 . "a") (2 . "b") (3 . "c"))))
+    (should (equal (update-or-add-alist 'abc 1 "a") '((1 . "a") (2 . "b") (3 . "c"))))
+    ))
+
+(ert-deftest listify-test-update-or-add-alist_same_car_element ()
+  "Test `update-or-add-alist'.
+Update alist when alist contains the element which car is same but cdr is different."
+  (let ((abc '((1 . "a") (2 . "b") (3 . "c"))))
+    (should (equal (update-or-add-alist 'abc 1 "A") '((1 . "A") (2 . "b") (3 . "c"))))
+    ))
+
+(ert-deftest listify-test-update-or-add-alist_same_car_cdr_element ()
+  "Test `update-or-add-alist'.
+Update alist when alist contains the element which car and cdr is same."
+  (let ((abc '((1 . "a") (2 . "b") (3 . "c"))))
+    (should (equal (update-or-add-alist 'abc 1 "a") '((1 . "a") (2 . "b") (3 . "c"))))
     ))
 
 ;;; listify-test.el ends here
