8089dfe6b6b5c7c85ec9e6f29a495077392e90f6
j8takagi <j8takagi@nifty.com>
2023-11-10 12:21:39 +0900

    jawordとtinysegmenterをサブモジュール化

--- site-lisp/jaword/set-compile.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'load-path default-directory)
-(add-to-list 'load-path (concat default-directory "/../tinysegmenter"))
-
-;; Local Variables:
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
