c3f446e517a0c704956e23abb27a2c7d4ca046b0
j8takagi <j8takagi@nifty.com>
2023-11-20 18:58:48 +0900

    jawordとtinysegmenterのMakefileなどを.manageで管理する

---
+++ site-lisp/.manage/jaword/set-compile.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path default-directory)
+(add-to-list 'load-path (concat default-directory "/../tinysegmenter"))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
