0bc4b37c416dbb37a66012838479b35dce211865
j8takagi <j8takagi@nifty.com>
2023-11-21 21:53:45 +0900

    *scratch*保存後に変更フラグをオフにする

--- site-lisp/continue-scratch/continue-scratch.el
+++ site-lisp/continue-scratch/continue-scratch.el
@@ -1,9 +1,9 @@
-;;; continue-scratch.el --- -*- lexical-binding: t -*-
+;;; continue-scratch.el -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2023 by Kazubito Takagi
 
 ;; Authors: Kazubito Takagi
-;; Keywords:
+;; Keywords: desktop scratch
 
 ;;; Commentary:
 
@@ -18,21 +18,31 @@
   "*scratch* buffer point to be saved.")
 
 (defun continue-scratch-save()
+  "Save current *Scratch* buffer
+string to variable `continue-scratch-buffer-string'
+and point to `continue-scratch-point', and
+return list of buffer string and point."
   (with-current-buffer "*scratch*"
     (setq
      continue-scratch-buffer-string (buffer-string)
      continue-scratch-point (point)
-     ))
+     )
+    (set-buffer-modified-p nil)
+    )
   `(,continue-scratch-buffer-string ,continue-scratch-point))
 
 (defun continue-scratch-load()
+  "Load buffer string and point to *Scratch* buffer
+from variable `continue-scratch-buffer-string' and
+`continue-scratch-point'."
   (with-current-buffer (get-buffer-create "*scratch*")
     (let ((curr (buffer-string)))
       (when (member curr '("" initial-scratch-message))
         (unless (equal curr "")
           (erase-buffer))
         (insert continue-scratch-buffer-string)
-        (goto-char continue-scratch-point)))))
+        (goto-char continue-scratch-point)
+        (set-buffer-modified-p nil)))))
 
 (mapc
  (lambda (var)
@@ -41,7 +51,6 @@
  '(continue-scratch-buffer-string continue-scratch-point))
 
 (add-hook 'desktop-save-hook 'continue-scratch-save)
-
 (add-hook 'desktop-after-read-hook 'continue-scratch-load)
 
 (provide 'continue-scratch)
