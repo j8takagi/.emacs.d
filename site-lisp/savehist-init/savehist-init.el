f7fa8e3e77bf0d4905b8f3070958c5bb580d8330
j8takagi <j8takagi@nifty.com>
2023-12-01 19:50:46 +0900

    savehist-initで、不要な履歴関数を削除するように

--- site-lisp/savehist-init/savehist-init.el
+++ site-lisp/savehist-init/savehist-init.el
@@ -11,7 +11,29 @@
 ;;; Code:
 (require 'savehist)
 
-(defun savehist-init-additional-variables (&optional file)
+(let ((newval (copy-sequence savehist-ignored-variables)))
+  (mapc
+   (lambda (var)
+     (unless (member var newval)
+       (push var newval)))
+    '(
+      load-history
+      :prompt-history
+      ))
+  (unless (equal savehist-ignored-variables newval)
+    (custom-set-variables `(savehist-ignored-variables ',newval))))
+
+(defun savehist-init-other-saved-variables ()
+  (append
+   (copy-sequence savehist-minibuffer-history-variables)
+   (when desktop-save-mode
+     (mapcar
+      (lambda (var)
+        (when (boundp var)
+          (copy-sequence (eval var))))
+      '(desktop-globals-to-save desktop-locals-to-save)))))
+
+(defun savehist-init-add-savehist-additional-variables (&optional file)
     "Set all history or ring variables,
 except variables in `desktop-globals-to-save',
 load-history and :prompt-history to savehist-additional-variables
@@ -23,12 +45,7 @@ so that variabels are saved to `savehist-file'."
        (lambda (elt)
          (setq histvars
                (delete elt histvars)))
-       (append
-        savehist-minibuffer-history-variables
-        (when (and desktop-save-mode (boundp 'desktop-globals-to-save))
-          desktop-globals-to-save)
-        '(:prompt-history load-history)
-        ))
+       (append (savehist-init-other-saved-variables) savehist-ignored-variables))
       (mapc
        (lambda (elt)
          (unless (member elt savehist-additional-variables)
@@ -47,10 +64,24 @@ so that variabels are saved to `savehist-file'."
            )))
       savehist-additional-variables))
 
+(defun savehist-init-remove-savehist-additional-variables (&optional file)
+  (ignore file)
+  (let ((newval (copy-sequence savehist-additional-variables)))
+    (mapc
+     (lambda (elt) (setq newval (delete elt newval)))
+     (append (savehist-init-other-saved-variables) savehist-ignored-variables))
+    (unless (equal newval savehist-additional-variables)
+      (custom-set-variables `(savehist-additional-variables ',newval)))))
+
+(defun savehist-init-set-savehist-additional-variables (&optional file)
+  (savehist-init-add-savehist-additional-variables file)
+  (savehist-init-remove-savehist-additional-variables file)
+  )
+
 (mapc
  (lambda (ahook)
-   (unless (memq 'savehist-init-additional-variables (eval ahook))
-     (add-hook ahook 'savehist-init-additional-variables)))
+   (unless (memq 'savehist-init-set-savehist-additional-variables (eval ahook))
+     (add-hook ahook 'savehist-init-set-savehist-additional-variables)))
  '(
    after-init-hook
    after-load-functions
