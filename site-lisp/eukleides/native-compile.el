3e7e3c579ec4bbce101afebe3834de49d7d97349
j8takagi <j8takagi@nifty.com>
2023-12-13 21:28:19 +0900

    eukleides/rdoc-mode/rubydbライブラリーを削除
    コンパイルエラー発生のため

--- site-lisp/eukleides/native-compile.el
+++
@@ -1,7 +0,0 @@
-(defun init-native-compile ()
-  (mapc (lambda (file) (native-compile file)) command-line-args-left))
-
-;; Local Variables:
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
